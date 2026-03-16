d53e191465c0b3549b5dc8d9754c0855e0d2dc86
nicolasplanas <nicolasplanasgomes27@gmail.com>
2026-03-16 00:26:19 -0300

    Habilitando projeto no render

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from routes import register_routes
 
 app = Flask(__name__)
 
+# CORS para comunicar com o frontend
+CORS(app)
+
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
